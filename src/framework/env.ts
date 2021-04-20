053a8c42493625ff4043126c1315797a1313f647
Ore Poran <oreporan@gmail.com>
2021-04-20 13:48:09 +0300

    envs

--- src/framework/env.ts
+++ src/framework/env.ts
@@ -1,7 +1,7 @@
 export const tazmanUrl = "https://tazman.co.il/minisite.app/yogawithedo";
-export const days = process.env.DAYS ?? '1,3';
-export const startTimes = process.env.START_TIMES ?? '18:00,19:45';
-export const usernames = process.env.USERNAMES ?? 'oreporan@gmail.com';
-export const passwords = process.env.PASSWORDS ?? 'yotamp86';
-export const clientNames = process.env.CLIENT_NAMES ?? 'אור פורן';
-export const clientIdNums = process.env.CLIENT_IDS ?? '026488858';
+export const days = process.env.DAYS;
+export const startTimes = process.env.START_TIMES;
+export const usernames = process.env.USERNAMES;
+export const passwords = process.env.PASSWORDS;
+export const clientNames = process.env.CLIENT_NAMES;
+export const clientIdNums = process.env.CLIENT_IDS;
