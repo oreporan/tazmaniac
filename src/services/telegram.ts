e239e8d03776de20ba111cf096589585b72054ef
Ore Poran <oreporan@gmail.com>
2021-04-21 19:29:01 +0300

    polling games

--- src/services/telegram.ts
+++ src/services/telegram.ts
@@ -3,12 +3,16 @@ import type User from "../entities/User";
 import { telegramToken } from "../framework/env";
 import { Course } from "../../types";
 
-const bot = new TelegramBot(telegramToken, { polling: true });
+const bot = new TelegramBot(telegramToken, { polling: false });
 
 const template = (course: Course) =>
   `**Message from Tazmaniac!** \n Signed you up for class ${course.course_meeting_description_id} \n **Date:** ${course.date} \n **Time:** ${course.start_time} - ${course.end_time} \n **Free places:** ${course.free_places}`;
 
-const sendMessage = (user: User, course: Course) => {
+const sendMessage = async (user: User, course: Course) => {
+  if (bot.isPolling()) {
+    await bot.stopPolling();
+  }
+  await bot.startPolling();
   const { telegramChatId, username } = user;
   if (!telegramChatId) {
     console.log(
@@ -16,7 +20,10 @@ const sendMessage = (user: User, course: Course) => {
     );
     return;
   }
-  return bot.sendMessage(telegramChatId, template(course), {parse_mode: 'Markdown'});
+  await bot.sendMessage(telegramChatId, template(course), {
+    parse_mode: "Markdown",
+  });
+  await bot.stopPolling();
 };
 
 export default sendMessage;
